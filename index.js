5dbceb4a109f5ecd6a4e66463d44989fde2ee8c7
gkumar-pandey <gautamshekhar078@gmail.com>
2024-01-12 17:31:33 +0530

    feat:added cors and helmet

--- index.js
+++ index.js
@@ -6,9 +6,25 @@ const {
   error404Handler,
   errorHandler,
 } = require("./middleware/ErrorHandler.middleware");
+const helmet = require("helmet");
 
 const app = express();
 app.use(express.json());
+const cors = require("cors");
+app.use(helmet());
+const allowedOrigins = ["http://localhost:3000"];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 
 connectDb();
 
@@ -20,9 +36,9 @@ app.use("/movies", movieRouter);
 app.use("/user", UsersRoute);
 
 //404 error handler
-app.use(error404Handler());
+app.use(error404Handler);
 // Global error handler
-app.use(errorHandler());
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Server is running at PORT:3000");
