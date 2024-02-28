97e12565f46afd748d4702f7e846c97c2c9106c7
gkumar-pandey <gautamshekhar078@gmail.com>
2024-02-28 06:44:56 +0530

    update

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,8 @@ const routes = express.Router();
 
 routes.use("/api", require("./api"));
 
+routes.get("/", (req, res) => {
+  res.status(200).json("Hello Express");
+});
+
 module.exports = routes;
